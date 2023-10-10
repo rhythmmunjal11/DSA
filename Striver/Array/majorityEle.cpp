6e3f749b0238163201ce7c8b77252391ccaa24b4
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2023-10-11 02:35:25 +0530

    majorityEle

---
+++ Striver/Array/majorityEle.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int cnt = 0;
+//         int ele;
+
+//         for(int i = 0 ; i < nums.size() ; i++){
+
+//             if(cnt==0){
+//                 ele = nums[i];
+//                 cnt++;
+//             }
+
+//             else if(nums[i] == ele){
+//                 cnt++;
+//             }
+
+//             else {
+//                 cnt--;
+//             }
+
+
+//         }
+
+//         int cnt1 = 0;
+
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if(nums[i] == ele) 
+//                 cnt1++;
+//         }
+
+//         if(cnt1 > (nums.size()/2) ){
+//             return ele;
+//         }
+
+
+//         return -1;
+
+
+
+
+//     }
+// };
