c7d010473b0c4a332f54218f551d7a04061ab952
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2023-11-17 00:15:52 +0530

    pointer

---
+++ Pointers/1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 3;
+    int c;
+    cout<<c<<endl;
+
+    int *p ; // bad practice 
+    cout<<p<<endl;
+    cout<<*p<<endl;
+
+    // cout<<*a;  
+
+    // int *ptr = &a;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;
+
+    // *ptr = 10;
+    // cout<<a<<endl;
+    // cout<<*ptr<<endl;
+
+    // ptr = &b;
+    // cout<<ptr<<endl;
+
+    // cout<<sizeof(ptr)<<endl;
+
+    return 0;
+
+}
