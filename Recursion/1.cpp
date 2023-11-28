80ae529d88d0970dd6e9d62c3a068da55515b368
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2023-11-29 00:08:17 +0530

    recursion

---
+++ Recursion/1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n : "<<endl;
+
+    cin>>n;
+
+    cout<<n<<endl;
+
+    int n1 ;
+    cin>>n1;
+
+    cout<<n1;
+
+    return 0;
+
+}
