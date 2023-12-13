10f15db7fa6b78827e15f199f8dc577ab6292c98
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2023-12-13 22:21:57 +0530

    moreQue

---
+++ Recursion/Assignment/removeOccSubArr.cpp
@@ -0,0 +1,23 @@
+// void removeRE(string& s ,  string& part){
+
+//         int found = s.find(part);
+
+//         if(found != string::npos){
+
+//             string left_part = s.substr(0 , found);
+//             string right_part = s.substr(found+part.size() , s.size());
+
+//             s = left_part + right_part;
+
+//             removeRE(s,part);
+
+//         }
+
+//         else{
+
+//             // base  case (means string s , now there is no occurence of part string)
+
+//             return;
+//         }
+
+//     }
