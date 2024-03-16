223bd7f91e6df408f3776940b9e53ee6a8d4f752
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-03-16 20:04:37 +0530

    quickSort

--- Backtracking/quickSort.cpp
+++ Backtracking/quickSort.cpp
@@ -1,10 +1,68 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int partition(int arr[], int s, int e) {
+  
+  int pivotIndex = s;
+  int pivotElement = arr[s];
+
+ 
+  int count = 0;
+  for(int i=s+1; i<=e; i++) {
+    if(arr[i] <= pivotElement) {
+      count++;
+    }
+  }
+  
+  int rightIndex = s + count;
+  swap(arr[pivotIndex], arr[rightIndex]);
+  pivotIndex = rightIndex;
+
+  int i = s;
+  int j = e;
+
+  while( i < pivotIndex && j > pivotIndex) {
+    while(arr[i] <= pivotElement) {
+      i++;
+    }
+    while( arr[j] > pivotElement){
+      j--;
+    }
+
     
+    if(i < pivotIndex && j > pivotIndex)
+      swap(arr[i],arr[j]);
+  }
+  return pivotIndex;
+}
 
-    return 0;
+void quickSort(int arr[], int s, int e) {
+  //base case
+  if(s >= e)
+    return ;
 
+  
+  int p = partition(arr, s, e);
+
+  
+  quickSort(arr, s, p-1);
+
+ 
+  quickSort(arr, p+1, e);
+
+}
+int main() {
+  int arr[] = {8,1,3,4,20,50,50,50,50,50,5,1,1,1,1,2,2,2,50,30};
+  int n = 20;
+
+  int s = 0;
+  int e = n-1;
+  quickSort(arr, s, e);
+
+  for(int i=0; i<n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
