dd13f233091b2f0bf8811f57b290b917012eee61
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-04-05 00:22:37 +0530

    new1

--- Backtracking/reviseMerge.cpp
+++ Backtracking/reviseMerge.cpp
@@ -1,45 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// time complexity of mergeSort is : O(nlogn) 
-
 void merge(int* arr , int s , int e){
 
     int mid = s + (e-s)/2;
 
     int len1 = mid - s + 1;
-    int len2 = e - mid;
+    int len2 = e-mid;
 
-    // creating arrays dynamically of len1 and len2 lengths
     int* left = new int[len1];
     int* right = new int[len2];
 
-    // copy values
+    int k = s;
 
-    int k = s;                  
-
-    for (int i = 0; i < len1; i++)
-    {
+    for(int i = 0 ; i < len1 ; i++){
         left[i] = arr[k];
         k++;
     }
-    
 
     k = mid + 1;
 
-    for (int i = 0; i < len2; i++)
-    {
+    for(int i = 0 ; i < len2 ; i++){
         right[i] = arr[k];
         k++;
     }
-    
 
-
-    // merge Two Sorted Arrays
     int leftIndex = 0;
     int rightIndex = 0;
     int mainArrayIndex = s;
-
 
     while (leftIndex < len1 && rightIndex < len2)
     {
@@ -50,7 +38,6 @@ void merge(int* arr , int s , int e){
         else{
             arr[mainArrayIndex++] = right[rightIndex++];
         }
-
     }
 
 
@@ -59,66 +46,73 @@ void merge(int* arr , int s , int e){
         arr[mainArrayIndex++] = left[leftIndex++];
     }
 
-
     while (rightIndex < len2)
     {
         arr[mainArrayIndex++] = right[rightIndex++];
     }
-
-
-    // delete right and left wala newly creted array 
-
-    // delete[] left;
-    // delete[] right;
     
     
-
-
-
 
 }
 
 
-void mergeSort(int* arr , int s , int e ){
+void mergeSort(int* arr , int s , int e){
 
     // base case
-    
-    // s > e (return )
-    // s==e means array is alredy sorted (return) means there is only one element in array , so we are sure that arr is sorted 
-
     if(s>=e){
         return;
     }
 
     int mid = s + (e-s)/2;
 
-    // left part 
+    //left part
     mergeSort(arr , s , mid);
 
     // right part
     mergeSort(arr , mid + 1 , e );
 
 
-    //mergeTwoSorted Arrays
-    merge(arr , s , e);
+    // merge
+    merge(arr , s , e );
 
 
 }
 
+
+
 int main()
 {
+    // int arr[15];
+
+    // for(int i = 0 ; i < 5 ; i++){
+    //     cin>>arr[i];
+    // }
+
+    // cout<<sizeof(arr)/sizeof(arr[0]);
+
+
+    // vector<int>arr;
+
+
+    // for(int i = 0 ; i < 5 ; i++){
+    //     cin>>arr[i];
+    // }
+
+    // cout<<arr.size();
+
+
     int arr[6] = {30,2,4,5,16,4};
 
     int s = 0;
     int e = 5;
 
-    mergeSort(arr, s , e);
+    mergeSort(arr ,s , e);
 
-    for (int i = 0; i < 6; i++)
-    {
+
+    for(int i = 0 ; i <= e ; i++){
         cout<<arr[i]<<" ";
     }
-    
+
 
     return 0;
 
