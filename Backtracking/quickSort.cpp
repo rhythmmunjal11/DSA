db48d86743f45fcd2ec235a22a90a7915867f383
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-04-12 01:56:42 +0530

    revise

--- Backtracking/quickSort.cpp
+++ Backtracking/quickSort.cpp
@@ -14,6 +14,9 @@ int partition(int arr[], int s, int e) {
     }
   }
   
+
+  // here rightIndex varible name means that the pivotElement has reached to its position jha pr vo hona chaiye tha 
+
   int rightIndex = s + count;
   swap(arr[pivotIndex], arr[rightIndex]);
   pivotIndex = rightIndex;
@@ -22,6 +25,7 @@ int partition(int arr[], int s, int e) {
   int j = e;
 
   while( i < pivotIndex && j > pivotIndex) {
+
     while(arr[i] <= pivotElement) {
       i++;
     }
@@ -29,11 +33,32 @@ int partition(int arr[], int s, int e) {
       j--;
     }
 
-    
     if(i < pivotIndex && j > pivotIndex)
       swap(arr[i],arr[j]);
+
+
+  // or we can do this in this way *********** 
+
+    // if(arr[i] <= pivotElement){
+    //   i++;
+    // }
+
+    // else if (arr[j] > pivotElement)
+    // {
+    //   j--;
+    // }
+
+    // else{
+    //   swap(arr[i++] , arr[j++]);
+    // }
+    
+
+    
+    
   }
+
   return pivotIndex;
+
 }
 
 void quickSort(int arr[], int s, int e) {
@@ -51,12 +76,19 @@ void quickSort(int arr[], int s, int e) {
   quickSort(arr, p+1, e);
 
 }
+
+
 int main() {
-  int arr[] = {8,1,3,4,20,50,50,50,50,50,5,1,1,1,1,2,2,2,50,30};
-  int n = 20;
+
+  // int arr[] = {8,1,3,4,20,50,50,50,50,50,5,1,1,1,1,2,2,2,50,30};
+  // int n = 20;
+
+  int arr[] = {6,1,20,30,8,5,60,5};
+  int n = 8;
 
   int s = 0;
   int e = n-1;
+
   quickSort(arr, s, e);
 
   for(int i=0; i<n; i++) {
---
+++ Backtracking/try.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int i = 0;
+
+    while (i < 2)
+    {
+        cout<<"ji "<<endl;
+    }
+    
+
+    return 0;
+
+}
