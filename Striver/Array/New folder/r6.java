56ae1431a5e50b672d5aed785a8c69153d08581f
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-05-02 19:03:42 +0530

    newww

---
+++ Striver/Array/New folder/r6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class r6 {
+
+    public static void main(String[] args){
+    String s1 = "abc";
+    String s2 = "abc";
+
+    if(s1.equals(s2)){
+        System.out.println("yes");
+    }
+}
+}
