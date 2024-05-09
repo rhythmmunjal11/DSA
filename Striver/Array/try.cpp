5b493b74402f0051e1237923b3876d0116727c35
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-05-09 19:40:41 +0530

    s1

---
+++ Striver/Array/try.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>arr{1,2,3,4,5};
+
+    // int n = 5;
+
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cin>>arr[i];
+    // }
+
+
+    cout<<arr.size()<<endl; // in vector there is arr.size() 
+
+    // it's not sizeof(arr) 
+
+    for(int i = 0 ; i < 7 ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    
+
+    return 0;
+
+}
